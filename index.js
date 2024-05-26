dec69ecef85c4f37f0959e013139b43b6c416d91
Gabriel <2021240@student.cct.ie>
2024-05-27 00:08:24 +0100

    Application created
    Created index.js with app set, app.listen and app.get to get index and test

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");// use express package
+// body parser middleware 
+// Reference: https://www.npmjs.com/package/body-parser
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const path = require('path');
+const WebSocket = require('ws');
+
+//const PORT = (3000); //use port 3000
+// either use port 3000 or process.argv[2] from command line specify
+const PORT = (process.argv[2] || 3000);
+
+// assign app to express - create app
+app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+// rendering with ejs 
+// Reference https://ejs.co/
+app.set("view engine", "ejs");
+
+// put in public folder by joining current tirectory with public
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs", { __dirname} );
+});
+
+// Listen on port 3000
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
