b5f3a1430cc7b54286e5cc33eac354379c654a44
Gabriel <2021240@student.cct.ie>
2024-05-29 21:30:18 +0100

    added action buttons and web socket
    - existing server upgraded with websocket
    - when initialising player we now first check if player is already initialised
    - added initial logic for websocket to server and client side
    - added start game, place monster and end turn buttons and js functions for them

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const express = require("express");// use express package
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const path = require('path');
-const http = require('http');
 const WebSocket = require('ws');
 
 //const PORT = (3000); //use port 3000
@@ -24,10 +23,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 // VARIABLES FOR GAME
 let gamesPlayed = 0;
 let playersStats = {};
-let startingBoard = createBoard();
-// Create HTTP server and WebSocket server
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+let board = createBoard();
+
+
+// Listen on port and assign server to httpServer
+const httpServer = app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
+
+// WEBSOCKET
+// Create WebSocket server without a http server so we can upgrade existing server to handle it
+const wsServer = new WebSocket.Server( { noServer: true } );
+// upgrading the server to handle websockets
+httpServer.on('upgrade', async (request, socket, head) => {
+    wsServer.handleUpgrade(request, socket, head, (ws) => {
+        wsServer.emit('connection', ws, request);
+    });
+});
 
 // FUNCTIONS
 /**
@@ -53,24 +65,44 @@ function createBoard(){
  * Function to set initial player stats 
  */
 function initializePlayer(player) {
-    
+    // check first if that player no exist before reseting
+    if (!playersStats[player]) {
         playersStats[player] = {
             gamesWon: 0,
             gamesLost: 0
         };
-    
+    }
 }
 
 // SET UP ROUTES
 app.get("/", (req, res) => {
-    res.render("index.ejs", { board: startingBoard, gamesPlayed, playersStats} );
+    res.render("index.ejs", { board, gamesPlayed, playersStats} );
+});
+
+// WEBSOCKET CONNECTIONS
+// make a connection
+wsServer.on('connection', (ws) => {
+    // On first connection
+    // log that user connected
+    console.log('A user connected');
+
+    // SEND INITIAL DATA
+    // set initial data as json, type of message is init, and send gameBoard, number of games played and player statistic
+    const initData = JSON.stringify({ type: 'init', board, gamesPlayed, playersStats })
+    ws.send(initData); // send init data
+
+    // HANDLE MESSAGES
+    ws.on('message', (message) => {
+        // messages logic
+    });
+
+
+    // HANDLE DISCONNECT
+    // When connection closes
+    ws.on('close', () => {
+        // log that user disconnected
+        console.log('A user disconnected');
+    });
 });
 
 
-
-
-
-// Listen on port 3000
-app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
-});
--- public/client.js
+++ public/client.js
@@ -1,6 +1,68 @@
+// SET VARIABLES
 // new websocket
 const url = "ws://localhost:3000";
 const wsServer = new WebSocket(url);
+wsServer.onopen = () => {
+    console.log('Connected to WebSocket server');
+};
+
+wsServer.onerror = (error) => {
+    console.log('WebSocket error:', error);
+    console.log('WebSocket error:', error.message);
+};
+
+wsServer.onclose = () => {
+    console.log('WebSocket connection closed');
+};
 
 // get game board
 const gameBoard = document.getElementById("gameBoard");
+
+let currentPlayer = null; // set variable for current player. initially is null
+let isGameStarted = false; // checks if game started
+let isGameOver = false; // check if game finished
+
+// EVENTS
+// This will handle web socket messages (events)
+wsServer.onmessage = (event) => {
+    // pasrse JSON to get data
+    const data = JSON.parse(event.data);
+    // if type sent is next turn 
+    if (data.type === 'nextTurn') {
+        // and if player is current player
+        if (data.player === currentPlayer) {
+            // display alert to current player that it is his turn
+            alert("It's your turn!");
+        }
+    }
+};
+
+// BUTTON ACTIONS
+/**
+ * Function to start/reset game
+ */
+function startGame(){
+    alert('start');
+    // enter name on start of game
+    const player = prompt("Enter your player name:");
+    isGameStarted = true; // set game started to true
+    // set data to be json with startGame type and set player 
+    const data = JSON.stringify({ type: 'startGame', player })
+    // send data to websocket
+    wsServer.send(data);
+}
+/**
+ * Function to end turn
+ */
+function endTurn(){
+    alert('endTurn');
+    // set data to be json with endTurn type and current player set as player
+    const data = JSON.stringify({ type: 'endTurn', player: currentPlayer });
+    // send data to websocket
+    wsServer.send(data);
+}
+
+function placeMonster(){
+    alert('placeMonster');
+    // ADD some logic for placing monsters on board
+}
